4ac17492d76a2c764f2659dae52ad89b9dc0ed09
gintsk <3540004+gintsk@users.noreply.github.com>
2024-02-14 19:13:18 +0200

    test: Code cleanup in test project (#82)
    * Minor code-style cleanups
    
    * Migrate test to System.Text.Json
    
    * Add supported dotnet versions to test project
    
    * Make tests async and simplify TestMimeDbMimeTypesAsync
    
    * Use https for svn.apache.org
    
    * Remove redundant usings
    
    * Update Test/Test.csproj
    
    Drop net7.0 as it will be out-of-support soon
    
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>
    
    * PR comments: revert changes and ValueTask fix (net48)
    
    * fix spacing
    
    * Update Test/Test.csproj
    
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>
    
    ---------
    
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- Test/TemplateSourceTests.cs
+++ Test/TemplateSourceTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json.Linq;
 
@@ -10,14 +11,14 @@ namespace Test
     [TestClass]
     public class TemplateSourceTests
     {
-        private const string APACHE_URL = "http://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types";
+        private const string APACHE_URL = "https://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types";
         private const string NGINX_URL = "https://raw.githubusercontent.com/h5bp/server-configs-nginx/master/mime.types";
         private const string MIMEDB_URL = "https://raw.githubusercontent.com/jshttp/mime-db/master/src/custom-types.json";
 
         [TestMethod]
-        public void TestApacheMimeTypes()
+        public async Task TestApacheMimeTypesAsync()
         {
-            var keyPairs = GetMimeTypesFromText(APACHE_URL, line =>
+            var keyPairs = await GetMimeTypesFromTextAsync(APACHE_URL, line =>
             {
                 if (line[0] == '#') return null;
                 var parts = line.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
@@ -28,9 +29,9 @@ namespace Test
         }
 
         [TestMethod]
-        public void TestNginxMimeTypes()
+        public async Task TestNginxMimeTypesAsync()
         {
-            var keyPairs = GetMimeTypesFromText(NGINX_URL, line =>
+            var keyPairs = await GetMimeTypesFromTextAsync(NGINX_URL, line =>
             {
                 line = line.Trim().TrimEnd(';');
                 if (line[0] == '#' || line[0] == '}' || line.StartsWith("types {")) return null;
@@ -42,12 +43,12 @@ namespace Test
         }
 
         [TestMethod]
-        public void TestMimeDbMimeTypes()
+        public async Task TestMimeDbMimeTypesAsync()
         {
-            var keyPairs = GetMimeTypesFromJson(MIMEDB_URL, resource =>
+            var keyPairs = await GetMimeTypesFromJsonAsync(MIMEDB_URL, resource =>
             {
                 return from mimeTypes in resource.Children()
-                       let mimeType = ((JProperty) mimeTypes).Name
+                       let mimeType = ((JProperty)mimeTypes).Name
                        from mimeTypeProperties in mimeTypes.Children()
                        from mimeTypeProperty in mimeTypeProperties.Children()
                        from mimeTypeValue in mimeTypeProperty.Values()
@@ -62,23 +63,23 @@ namespace Test
             Assert.IsTrue(keyPairs.Any());
         }
 
-        private static string GetPageContent(string url)
+        private static async Task<string> GetPageContentAsync(string url)
         {
             using var client = new HttpClient();
-            return client.GetStringAsync(url).GetAwaiter().GetResult();
+            return await client.GetStringAsync(url);
         }
 
-        private static IEnumerable<string[]> GetMimeTypesFromText(string url, Func<string, string[]> processLine)
+        private static async Task<IEnumerable<string[]>> GetMimeTypesFromTextAsync(string url, Func<string, string[]> processLine)
         {
-            var content = GetPageContent(url);
+            var content = await GetPageContentAsync(url);
             if (string.IsNullOrEmpty(content)) return Enumerable.Empty<string[]>();
             var lines = content.Split(new[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
             return lines.Select(processLine).Where(newKeyPairs => newKeyPairs != null).ToList();
         }
 
-        private static IEnumerable<string[]> GetMimeTypesFromJson(string url, Func<JObject, IEnumerable<string[]>> processObject)
+        private static async Task<IEnumerable<string[]>> GetMimeTypesFromJsonAsync(string url, Func<JObject, IEnumerable<string[]>> processObject)
         {
-            var content = GetPageContent(url);
+            var content = await GetPageContentAsync(url);
             if (string.IsNullOrEmpty(content)) return Enumerable.Empty<string[]>();
             var resource = JObject.Parse(content);
             return processObject(resource);
