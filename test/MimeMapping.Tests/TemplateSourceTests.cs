decc144f93bf80551cbc208eeb4cb38ee59c4e42
Matthew Little <zone117x@gmail.com>
2026-01-17 11:25:06 -0700

    chore: generate KnownMimeTypes.cs on build

--- test/MimeMapping.Tests/TemplateSourceTests.cs
+++ test/MimeMapping.Tests/TemplateSourceTests.cs
@@ -2,35 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json.Linq;
 
 namespace Test
 {
     [TestClass]
     public class TemplateSourceTests
     {
-        private const string MIMEDB_URL = "https://raw.githubusercontent.com/jshttp/mime-db/v1.53.0/db.json";
+        private const string MIMEDB_URL = "https://raw.githubusercontent.com/jshttp/mime-db/v1.54.0/db.json";
 
         [TestMethod]
         public async Task TestMimeDbMimeTypesAsync()
         {
-            var keyPairs = await GetMimeTypesFromJsonAsync(MIMEDB_URL, resource =>
-            {
-                return from mimeTypes in resource.Children()
-                       let mimeType = ((JProperty)mimeTypes).Name
-                       from mimeTypeProperties in mimeTypes.Children()
-                       from mimeTypeProperty in mimeTypeProperties.Children()
-                       from mimeTypeValue in mimeTypeProperty.Values()
-                       where (mimeTypeProperty as JProperty)?.Name == "extensions"
-                       select new[]
-                       {
-                           mimeType,
-                           mimeTypeValue.Value<string>()
-                       };
-            });
-
+            var keyPairs = await GetMimeTypesFromJsonAsync(MIMEDB_URL);
             Assert.IsTrue(keyPairs.Any());
         }
 
@@ -40,20 +26,27 @@ namespace Test
             return await client.GetStringAsync(url);
         }
 
-        private static async Task<IEnumerable<string[]>> GetMimeTypesFromTextAsync(string url, Func<string, string[]> processLine)
+        private static async Task<IEnumerable<string[]>> GetMimeTypesFromJsonAsync(string url)
         {
             var content = await GetPageContentAsync(url);
             if (string.IsNullOrEmpty(content)) return Enumerable.Empty<string[]>();
-            var lines = content.Split(new[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
-            return lines.Select(processLine).Where(newKeyPairs => newKeyPairs != null).ToList();
-        }
 
-        private static async Task<IEnumerable<string[]>> GetMimeTypesFromJsonAsync(string url, Func<JObject, IEnumerable<string[]>> processObject)
-        {
-            var content = await GetPageContentAsync(url);
-            if (string.IsNullOrEmpty(content)) return Enumerable.Empty<string[]>();
-            var resource = JObject.Parse(content);
-            return processObject(resource);
+            using var doc = JsonDocument.Parse(content);
+            var results = new List<string[]>();
+
+            foreach (var mimeTypeEntry in doc.RootElement.EnumerateObject())
+            {
+                var mimeType = mimeTypeEntry.Name;
+                if (mimeTypeEntry.Value.TryGetProperty("extensions", out var extensions))
+                {
+                    foreach (var ext in extensions.EnumerateArray())
+                    {
+                        results.Add(new[] { mimeType, ext.GetString()! });
+                    }
+                }
+            }
+
+            return results;
         }
     }
 }
